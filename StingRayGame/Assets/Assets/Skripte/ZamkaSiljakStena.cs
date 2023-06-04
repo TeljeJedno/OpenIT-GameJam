d1ec284f5fc7172c03404103015a88d0ae4fd9a1
BoskoJDev <117722529+BoskoJDev@users.noreply.github.com>
2023-06-04 09:53:35 +0200

    azuriranje nivoa 1

--- StingRayGame/Assets/Assets/Skripte/ZamkaSiljakStena.cs
+++ StingRayGame/Assets/Assets/Skripte/ZamkaSiljakStena.cs
@@ -1,17 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ZamkaSiljakStena : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject siljakStena;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
