9cad74df74389c1345d7da7695162f00d8e899a7
BoskoJDev <117722529+BoskoJDev@users.noreply.github.com>
2023-06-04 13:31:29 +0200

    s

---
+++ StingRayGame/Assets/Assets/Skripte/ButtonCollider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Canvas canvas;
+    public void HideCanvas(){
+        canvas.enabled = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
+
+
+
+
+
+
